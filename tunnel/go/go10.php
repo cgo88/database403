d8684022935e4270339f6720bd84187f5b0af5a0
cgo88 <cgo88official@Gmail.com>
2024-12-22 02:32:11 +0700

    Update and rename go11.php to go10.php

--- tunnel/go/go10.php
+++ tunnel/go/go10.php
@@ -62,7 +62,7 @@ $templatePaths = [
 ];
 
 // Path dari list.txt lokal
-$listPath = __DIR__ . '/list11.txt';
+$listPath = __DIR__ . '/list10.txt';
 
 // Memproses file list.txt utama
 processList($listPath, $templatePaths);
